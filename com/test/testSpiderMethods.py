e4fb796c5aa9b6ec3b575de4271d3d3ca8b190fa
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-02-23 07:58:35 -0600

    Fixed mock in spider method as well.

--- com/test/testSpiderMethods.py
+++ com/test/testSpiderMethods.py
@@ -4,30 +4,34 @@ from com.src.Spider import Spider
 from com.src.network.ApiRequester import ApiRequester
 from unittest.mock import MagicMock
 from com.test.testUtil import *
+from unittest import mock
+
 
 class TestSpiderMethods(unittest.TestCase):
-    mock_api_requester = ApiRequester()
 
-    def testProcessSoup(self):
-        self.mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
-        processor = CryptoProcessor(self.mock_api_requester)
+    @mock.patch('com.src.network.ApiRequester')
+    def testProcessSoup(self, mock_api_requester):
+        mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
+        processor = CryptoProcessor(mock_api_requester)
         spider = Spider(5, "", processor)
         spider.pageSource = generateSimpleTestHTML3Mentions()
         spider.process_soup()
 
         self.assertEqual(len(spider.processor.processor_dict.keys()), 3)
-
-    def testProcessSoupEmptySource(self):
-        self.mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
-        processor = CryptoProcessor(self.mock_api_requester)
+    
+    @mock.patch('com.src.network.ApiRequester')
+    def testProcessSoupEmptySource(self, mock_api_requester):
+        mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
+        processor = CryptoProcessor(mock_api_requester)
         spider = Spider(5, "", processor)
         spider.pageSource = ""
         spider.process_soup()
         self.assertEqual(len(spider.processor.processor_dict.keys()), 0)
 
-    def testProcessSoupNullSource(self):
-        self.mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
-        processor = CryptoProcessor(self.mock_api_requester)
+    @mock.patch('com.src.network.ApiRequester')
+    def testProcessSoupNullSource(self, mock_api_requester):
+        mock_api_requester.get = MagicMock(return_value=generateTestCoinJSONResponse())
+        processor = CryptoProcessor(mock_api_requester)
         spider = Spider(5, "", processor)
         spider.pageSource = None
         spider.process_soup()
