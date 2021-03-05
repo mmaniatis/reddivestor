0997a4256a1d626c5df533e20bf5c56696ebbe85
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-03-05 11:35:41 -0600

    6-Reverting.

--- main.py
+++ main.py
@@ -8,39 +8,39 @@ import time
 
 #TODO: Need to fix init.. put this into a config for sure.
 
-# def init_processor():
-#     api_requester = ApiRequester()
-#     mongo_datastore = MongoDatastore()
-#     return CryptoProcessor(api_requester, mongo_datastore)
+def init_processor():
+    api_requester = ApiRequester()
+    mongo_datastore = MongoDatastore()
+    return CryptoProcessor(api_requester, mongo_datastore)
 
-# processor = init_processor()
-# processor.populate_coin_list_offline()
-# processor.populate_seen_post_titles()
+processor = init_processor()
+processor.populate_coin_hash()
+processor.populate_seen_post_titles()
 
 def engine_start():
     interval = 2000 #milliseconds
 
     print("!!!! Spider Engine starting, initializing processors and spiders !!!!")
-    # spider_cryptoCurrency = Spider(interval, 'https://www.reddit.com/r/CryptoCurrencies/new/', processor)
-    # spider_cryptoCurrency.setUpDriver()
-    # spider_cryptoCurrencyTrading = Spider(interval, 'https://www.reddit.com/r/CryptoCurrencyTrading/new/', processor)
-    # spider_cryptoCurrencyTrading.setUpDriver()
-    # spider_cryptoCryptoMarkets = Spider(interval, 'https://www.reddit.com/r/CryptoMarkets/new/', processor)
-    # spider_cryptoCryptoMarkets.setUpDriver()
+    spider_cryptoCurrency = Spider(interval, 'https://www.reddit.com/r/CryptoCurrencies/new/', processor)
+    spider_cryptoCurrency.setUpDriver()
+    spider_cryptoCurrencyTrading = Spider(interval, 'https://www.reddit.com/r/CryptoCurrencyTrading/new/', processor)
+    spider_cryptoCurrencyTrading.setUpDriver()
+    spider_cryptoCryptoMarkets = Spider(interval, 'https://www.reddit.com/r/CryptoMarkets/new/', processor)
+    spider_cryptoCryptoMarkets.setUpDriver()
 
-    # thread = threading.Thread(target=spider_cryptoCurrency.crawlAndRefresh, args=())
-    # thread.daemon = True    
+    thread = threading.Thread(target=spider_cryptoCurrency.crawlAndRefresh, args=())
+    thread.daemon = True    
 
-    # thread2 = threading.Thread(target=spider_cryptoCurrencyTrading.crawlAndRefresh, args=())
-    # thread2.daemon = True
+    thread2 = threading.Thread(target=spider_cryptoCurrencyTrading.crawlAndRefresh, args=())
+    thread2.daemon = True
 
-    # thread3 = threading.Thread(target=spider_cryptoCryptoMarkets.crawlAndRefresh, args=())
-    # thread3.daemon = True
+    thread3 = threading.Thread(target=spider_cryptoCryptoMarkets.crawlAndRefresh, args=())
+    thread3.daemon = True
 
 
-    # thread.start()
-    # thread2.start()
-    # thread3.start()
+    thread.start()
+    thread2.start()
+    thread3.start()
        
 
     
@@ -49,7 +49,7 @@ def main():
     engine_start()
     while(True):
         print("Engine is up and running...")
-        time.sleep(10)
+        time.sleep(500)
 
 if __name__ == "__main__":
     main()
